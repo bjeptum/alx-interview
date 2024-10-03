20bc8ea23acb594633ea35ac27581fa93b7452e2
b_jeptum <brendajeptum@gmail.com>
2024-10-04 00:18:56 +0300

    Solved pascal's triangle algorithm exercise

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+import math
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the PAscal's triangle of n
+    """
+    if not isinstance(n, int):
+        raise TypeError("Input must be an integer.")
+    if n <= 0:
+        raise ValueError("Input must be a positive integer.")
+
+    tri = []
+
+    for row in range(n):
+        current_row = [1]  # First element is always 1
+
+        if row > 0:
+            for colum in range(1, row):
+                value = tri[row - 1][colum - 1] + tri[row - 1][colum]
+                current_row.append(value)
+            current_row.append(1)  # Last element is always 1
+
+        tri.append(current_row)
+
+    return tri
